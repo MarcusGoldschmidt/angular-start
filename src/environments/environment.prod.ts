1aab103871db6ffa291e0d25b028d2b5be196cc3
Marcus Goldschmidt Oliveira <38927366+MarcusGoldschmidt@users.noreply.github.com>
2018-11-24 18:03:23 -0300

    Corrigido variáveis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyBucI6HVPPiiFD6EcOWyKIAWLJvwNsmhHs",
-    authDomain: "meus-gastos-e1a19.firebaseapp.com",
-    databaseURL: "https://meus-gastos-e1a19.firebaseio.com",
-    projectId: "meus-gastos-e1a19",
-    storageBucket: "meus-gastos-e1a19.appspot.com",
-    messagingSenderId: "106046534392"
-  }
+  production: true
 };
