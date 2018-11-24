5e0790d48e460d3b9655548c94aa16fe28a6807a
Marcus Goldschmidt <marcus.golds@hotmail.com>
2018-11-24 11:11:58 -0300

    Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fist-angular';
+  name = 'Marcus Goldschmidt Oliveira';
+  text = '';
+
+  exibirAlerta() {
+    alert(this.name);
+  }
 }
